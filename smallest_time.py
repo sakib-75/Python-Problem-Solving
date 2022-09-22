bc6b0cccbdff603b4f81eb1ab2038759b9ab0fe9
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-09-22 20:40:27 +0600

    Create smallest_time.py

---
+++ smallest_time.py
@@ -0,0 +1,64 @@
+import functools
+
+
+def smallest_time():
+    """
+        This method return smallest time from a list of given time in a day
+    """
+    all_visit_time = ['12:30 PM', '1:22 AM', '3:04 AM']
+    am_12h_time = []
+    am_time = []
+    pm_12h_time = []
+    pm_time = []
+
+    for visit_time in all_visit_time:
+        if 'AM' in visit_time:
+            if '12' in visit_time:
+                am_12h_time.append(visit_time.split(' ')[0])
+            else:
+                am_time.append(visit_time.split(' ')[0])
+        elif 'PM' in visit_time:
+            if '12' in visit_time:
+                pm_12h_time.append(visit_time.split(' ')[0])
+            else:
+                pm_time.append(visit_time.split(' ')[0])
+
+    print(am_12h_time, am_time, pm_12h_time, pm_time)
+
+    def smallest_12h_time(_12h_time, am_pm):
+        minute_list = list(map(lambda hour_minute: int(hour_minute.split(':')[1]), _12h_time))
+        smallest_minute = functools.reduce(lambda m1, m2: m1 if m1 < m2 else m2, minute_list)
+        return f'12:{str(smallest_minute).zfill(2)} {am_pm}'
+
+    def smallest_time(time1, time2):
+        time1_hour = int(time1.split(':')[0])
+        time1_minute = int(time1.split(':')[1])
+        time2_hour = int(time2.split(':')[0])
+        time2_minute = int(time2.split(':')[1])
+
+        if time1_hour == time2_hour:
+            if time1_minute < time2_minute:
+                return time1
+            else:
+                return time2
+        elif time1_hour < time2_hour:
+            return time1
+        else:
+            return
+
+    if len(am_12h_time) > 0:
+        return smallest_12h_time(am_12h_time, 'AM')
+
+    elif len(am_time) > 0:
+        smallest_am_time = functools.reduce(lambda t1, t2: smallest_time(t1, t2), am_time)
+        return f'{smallest_am_time} AM'
+
+    elif len(pm_12h_time) > 0:
+        return smallest_12h_time(pm_12h_time, 'PM')
+
+    elif len(pm_time) > 0:
+        smallest_pm_time = functools.reduce(lambda t1, t2: smallest_time(t1, t2), pm_time)
+        return f'{smallest_pm_time} PM'
+
+
+print("Smallest time: ", smallest_time())
