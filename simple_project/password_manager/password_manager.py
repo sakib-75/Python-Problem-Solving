6f586be3fd9391cd538aaf2822299e8a33ac0109
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-10-03 17:15:39 +0600

    Update password_manager.py

--- simple_project/password_manager/password_manager.py
+++ simple_project/password_manager/password_manager.py
@@ -1,19 +1,23 @@
 from cryptography.fernet import Fernet
 
 
-def view():
-    print('\nView existing password')
+def view_password():
     with open('passwords.txt', 'r') as file:
-        for i, line in enumerate(file.readlines()):
-            data = line.rstrip()
-            key, user, passw = data.split('|')
-            fernet = Fernet(key.encode())
-            decrypt_user = fernet.decrypt(user.encode()).decode()
-            decrypt_pass = fernet.decrypt(passw.encode()).decode()
-            print(i + 1, 'Username:', decrypt_user, ',', 'Password:', decrypt_pass)
+        lines = file.readlines()
+        if len(lines) > 0:
+            print('\n...View existing password...')
+            for i, line in enumerate(lines):
+                data = line.rstrip()
+                key, user, passw = data.split('|')
+                fernet = Fernet(key.encode())
+                decrypt_user = fernet.decrypt(user.encode()).decode()
+                decrypt_pass = fernet.decrypt(passw.encode()).decode()
+                print(i + 1, 'Username:', decrypt_user, ',', 'Password:', decrypt_pass)
+        else:
+            print("No password available!")
 
 
-def add():
+def add_password():
     username = input('Username: ')
     password = input('Password: ')
     key = Fernet.generate_key()
@@ -22,12 +26,21 @@ def add():
     encrypt_password = fernet.encrypt(password.encode()).decode()
     with open('passwords.txt', 'a') as file:
         file.write(key.decode() + '|' + encrypt_username + '|' + encrypt_password + '\n')
+    print('Password added!')
+
+
+def remove_all_password():
+    file = open('passwords.txt', 'r+')
+    file.truncate(0)
+    file.close()
+    print('All password removed!')
 
 
 while True:
     print("\n")
     print("Enter v for view")
     print("Enter a for add")
+    print("Enter r for remove all password")
     print("Enter q for quit")
 
     option = input('Enter your option: ').lower()
@@ -35,8 +48,10 @@ while True:
     if option == 'q':
         break
     elif option == 'v':
-        view()
+        view_password()
     elif option == 'a':
-        add()
+        add_password()
+    elif option == 'r':
+        remove_all_password()
     else:
         print('Invalid option')
