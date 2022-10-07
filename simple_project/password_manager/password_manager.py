04708e3d0eeb5142c5cd30e20cec089b235e0b7d
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-10-07 21:25:18 +0600

    Update password_manager.py

--- simple_project/password_manager/password_manager.py
+++ simple_project/password_manager/password_manager.py
@@ -34,28 +34,83 @@ def add_password():
 
 
 def remove_all_password():
-    file = open('passwords.txt', 'r+')
-    file.truncate(0)
-    file.close()
+    with open('passwords.txt', 'r+') as file:
+        file.truncate(0)
+
+    # file = open('passwords.txt', 'r+')
+    # file.truncate(0)
+    # file.close()
+
     print('All password removed!')
 
 
 while True:
-    print("\n")
-    print("Enter v for view")
-    print("Enter a for add")
-    print("Enter r for remove all password")
-    print("Enter q for quit")
+    print('\n')
+    print('...Login Page...')
+    print('[1] Login')
+    print('[2] Register')
+    print('[3] Quit')
+    option = input('Enter your option: ')
 
-    option = input('Enter your option: ').lower()
+    if option == '1':
+        is_login = False
+        login_username = input('type your username to login:')
+        login_password = input('type your password to login:')
 
-    if option == 'q':
+        with open('login.txt', 'r') as file:
+            lines = file.readlines()
+            if len(lines) > 0:
+                for line in lines:
+                    data = line.strip()
+                    key, username, password = data.split(',')
+                    fernet = Fernet(key.encode())
+                    decrypt_user = fernet.decrypt(username.encode()).decode()
+                    decrypt_pass = fernet.decrypt(password.encode()).decode()
+                    if login_username == decrypt_user and login_password == decrypt_pass:
+                        is_login = True
+                        break
+
+                if is_login:
+                    print('Login success!')
+                else:
+                    print('Invalid username or password!')
+            else:
+                print('No registered user available!')
+
+        while is_login:
+            print("\n")
+            print('...Password Page...')
+            print("Enter v for view password")
+            print("Enter a for add password")
+            print("Enter r for remove all password")
+            print("Enter q for quit")
+
+            option = input('Enter your option: ').lower()
+
+            if option == 'q':
+                break
+            elif option == 'v':
+                view_password()
+            elif option == 'a':
+                add_password()
+            elif option == 'r':
+                remove_all_password()
+            else:
+                print('Invalid option')
+
+    elif option == '2':
+        username = input('Username for register: ')
+        password = input('Password for register: ')
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+        encrypt_username = fernet.encrypt(username.encode()).decode()
+        encrypt_password = fernet.encrypt(password.encode()).decode()
+        with open('login.txt', 'a') as file:
+            file.write(key.decode() + ',' + encrypt_username + ',' + encrypt_password + '\n')
+        print('User added!')
+
+    elif option == '3':
         break
-    elif option == 'v':
-        view_password()
-    elif option == 'a':
-        add_password()
-    elif option == 'r':
-        remove_all_password()
+
     else:
         print('Invalid option')
